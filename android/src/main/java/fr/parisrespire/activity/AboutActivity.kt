e496b15eacddb7ab8a8c2be3d23bf8503eded8ae
Alan Camilo <alan.c.herrera@gmail.com>
2020-04-17 23:29:39 +0200

    adding a back button in AboutActivity and NotificationSettingsActivity

--- android/src/main/java/fr/parisrespire/activity/AboutActivity.kt
+++ android/src/main/java/fr/parisrespire/activity/AboutActivity.kt
@@ -1,6 +1,9 @@
 package fr.parisrespire.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import fr.parisrespire.R
 
@@ -9,5 +12,15 @@ class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+        val actionBar: ActionBar? = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+        actionBar?.title = null
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+        startActivity(intent)
+        return true
     }
 }
--- android/src/main/java/fr/parisrespire/activity/NotificationSettingsActivity.kt
+++ android/src/main/java/fr/parisrespire/activity/NotificationSettingsActivity.kt
@@ -1,9 +1,12 @@
 package fr.parisrespire.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.CheckBox
 import android.widget.TimePicker
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import fr.parisrespire.R
 import fr.parisrespire.fragment.TimePickerFragment
@@ -11,6 +14,7 @@ import fr.parisrespire.presenter.NotificationSettingsPresenter
 import kotlinx.android.synthetic.main.activity_notification_settings.*
 import org.joda.time.DateTime
 import org.joda.time.DateTimeZone
+
 
 const val timePreference = "time_preference"
 const val notificationPreference = "notification_preference"
@@ -28,6 +32,16 @@ class NotificationSettingsActivity : AppCompatActivity() {
         btn_time_picker.isEnabled = presenter.getNotifyPreference()
         checkbox_alerts.isChecked = presenter.getAlertPreference()
         checkbox_notifications.isChecked = presenter.getNotifyPreference()
+        val actionBar: ActionBar? = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+        actionBar?.title = null
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+        startActivity(intent)
+        return true
     }
 
     fun onCheckBoxClicked(view: View) {
