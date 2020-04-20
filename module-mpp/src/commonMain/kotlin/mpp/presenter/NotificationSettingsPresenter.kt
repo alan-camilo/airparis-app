f848b61632d70558fc2f6b05447fa47a94d4246f
Alan Camilo <alan.c.herrera@gmail.com>
2020-04-20 16:01:08 +0200

    NotificationSettingsPresenter interface added in the mpp-module

---
+++ module-mpp/src/commonMain/kotlin/mpp/presenter/NotificationSettingsPresenter.kt
@@ -0,0 +1,17 @@
+package mpp.presenter
+
+interface NotificationSettingsPresenter {
+
+    fun setNotifyPreference(value: Boolean)
+
+    fun setAlertPreference(value: Boolean)
+
+    fun setTimePreference(timeInMillis: Long)
+
+    fun getTimeHour(): String
+
+    fun getNotifyPreference(): Boolean
+
+    fun getAlertPreference(): Boolean
+
+}
